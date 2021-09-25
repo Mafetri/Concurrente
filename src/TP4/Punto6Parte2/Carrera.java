9849f5c229e243ca065444d7d8fcab4029d8f9e7
Manuel Triñanes <manutritri2@gmail.com>
2021-09-25 18:52:45 -0300

    Punto6 Parte2 ok!

--- src/TP4/Punto6Parte2/Carrera.java
+++ src/TP4/Punto6Parte2/Carrera.java
@@ -2,22 +2,32 @@ package TP4.Punto6Parte2;
 import java.util.concurrent.*;
 
 public class Carrera {
-    private Semaphore testigo = new Semaphore(1);
+    int cantCorredores;
+    private Semaphore[] testigo;
     private int posSiguienteTestigo = 0;
 
+    public Carrera(int cantCorredores) {
+        testigo = new Semaphore[cantCorredores];
+        testigo[0] = new Semaphore(1);
+        for(int i = 1; i <testigo.length; i++) {
+            testigo[i] = new Semaphore(0);
+        }
+    }
+
     public void correr(int posActual) throws InterruptedException {
-        do{
-            testigo.acquire();
-            if(posSiguienteTestigo != posActual){
-                testigo.release();
-            }
-        }while(posSiguienteTestigo != posActual);
+        testigo[posActual].acquire();
         System.out.println("-> " + Thread.currentThread().getName() + " agarró el testigo.");
     }
 
     public void soltar(){
-        testigo.release();
-        System.out.println(Thread.currentThread().getName() + " solto el testigo.");
         posSiguienteTestigo++;
+        if(posSiguienteTestigo < testigo.length){
+            testigo[posSiguienteTestigo].release();
+            System.out.println(Thread.currentThread().getName() + " solto el testigo.");
+        }else{
+            System.out.println("Terminó la carrera");
+        }
+        
+        
     }
 }
--- src/TP4/Punto6Parte2/Main.java
+++ src/TP4/Punto6Parte2/Main.java
@@ -1,15 +1,39 @@
 package TP4.Punto6Parte2;
 
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args){
-        String[] nombres = {"José", "Manuel", "Ulises", "Bruno"};
-        Carrera carrera = new Carrera();
-        
-        Thread[] corredores = new Thread[nombres.length];
-    
-        for(int i = 0; i < corredores.length; i++){
-            corredores[i] = new Thread(new Atleta(i, carrera), nombres[i]);
+    public static void main(String[] args) {
+        int cantCorredores = 10;
+        Carrera carrera = new Carrera(cantCorredores);
+        Thread[] corredores = new Thread[cantCorredores];
+
+        // Todo esto para que Vale este feliz con el arreglo shuffleado
+        int[] posicion = new int[cantCorredores];
+        for (int i = 0; i < posicion.length; i++) {
+            posicion[i] = i;
+        }
+        shuffleArray(posicion);
+        for (int i = 0; i < posicion.length; i++) {
+            System.out.print(posicion[i]);
+        }
+
+        for (int i = 0; i < corredores.length; i++) {
+            corredores[i] = new Thread(new Atleta(posicion[i], carrera), "Corredor " + i);
             corredores[i].start();
         }
     }
+
+    static void shuffleArray(int[] ar) {
+        // If running on Java 6 or older, use `new Random()` on RHS here
+        Random rnd = new Random();
+        for (int i = ar.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            int a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
+        }
+    }
+
 }
